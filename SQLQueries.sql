cb14a45d0a0c4accfe75c59c8560f2679c3f371b
Gian Clarke <gianclarke@hotmail.com>
2021-08-27 12:27:23 +1200

    Completed QSL Queries

--- SQLQueries.sql
+++ SQLQueries.sql
@@ -1,9 +1,6 @@
 USE Hospital
 GO
 
-SELECT DATEDIFF(day, [ReferralDate], [FSADate]) FROM [dbo].[Referral];
-
-
 SET statistics io ON
 GO
 
@@ -17,11 +14,12 @@ CAST(DATEDIFF(DAY,p.DOB,r.referralDate) /365.25 AS INT) AS 'Patient Age',
 DATEDIFF(day, r.[ReferralDate], r.[FSADate]) AS 'Days Waiting'
 FROM [dbo].[Referral] r
 INNER JOIN [dbo].[Patient]p ON r.[PatientID]=p.[PatientID]
+WHERE r.[HealthTargetEligible] = 1
 
 -- 1.How many people have been referred for cardiothoracic?
 SELECT COUNT(r.[DepartmentID]) AS 'Total Referred'
 FROM [dbo].[Referral] r
-WHERE r.[DepartmentID] = 2
+WHERE r.[DepartmentID] = 2 AND r.[HealthTargetEligible] = 1
 
 
 -- 2. What is the average time taken (in days) to see a Surgeon by Department?
@@ -35,20 +33,19 @@ ORDER BY d.[DepartmentName] ASC
 
 SELECT s.[SurgeonName] AS 'Surgeon Name', 
 CONCAT(p.[PatientFName], ' ',p.[PatientLName] ) AS 'Patient Name',
-ISNULL(DATEDIFF(day, r.[ReferralDate], r.[FSADate]), 0) AS 'Days Waiting'
+DATEDIFF(day, r.[ReferralDate], r.[FSADate]) AS 'Days Waiting'
 FROM [dbo].[Referral] r
 INNER JOIN [dbo].[Patient] p ON p.[PatientID] = r.[PatientID]
 INNER JOIN [dbo].[Surgeon] s ON s.[SurgeonID] = r.[SurgeonID]
 ORDER BY s.SurgeonName ASC
 -- WHERE r.[FSADate] IS NOT NULL
 
-
 -- 4. Assuming that all patients under 18 need to be seen by Paediatric Surgery, are there any patients who need to be reassigned? 
+SELECT p.[PatientID] AS 'Patient NHI',CONCAT(p.[PatientFName], ' ', p.[PatientLName]) AS 'Patient Name',CAST(DATEDIFF(DAY,p.[DOB],r.[ReferralDate]) /365.25 AS INT) AS 'Patient Age',d.[DepartmentName] AS 'Department Name'FROM  [dbo].[Referral] rINNER JOIN [dbo].[Patient] p ON p.[PatientID]=r.[PatientID]INNER JOIN [dbo].[Department] d ON r.[DepartmentID]=d.[DepartmentID]WHERE CAST(DATEDIFF(DAY,p.[DOB],r.[ReferralDate]) /365.25 AS INT) < 18 AND r.[HealthTargetEligible] = 1 -- 1 = yes 0 = noAND d.[DepartmentName] !='Paediatric Surgery'
+
+
 
 -- 5.What percentage of patient were seen within the target of 75 days by department?
-SELECT r.[DepartmentID], (case when DATEDIFF(day, r.[ReferralDate], r.[FSADate]) < 76 then 1 else null end) AS 'Days Waiting'
-FROM [dbo].[Referral] r
-INNER JOIN [dbo].[Department] d ON d.[DepartmentID] = r.[DepartmentID]
-WHERE r.[FSADate] IS NOT NULL
-ORDER BY r.[DepartmentID]
+SELECT d.[DepartmentName] AS 'Department Name',CAST(CAST(m.MetCount AS DECIMAL) / CAST(p.NoPatient AS DECIMAL) *100 AS DECIMAL) AS 'Percentage Met'FROM(	SELECT r.[DepartmentID], COUNT(*) AS MetCount 	FROM  [dbo].[Referral] r	WHERE DATEDIFF(DAY,r.[ReferralDate],r.[FSADate]) < 76	GROUP BY r.[DepartmentID]) AS mRIGHT JOIN (	SELECT r.[DepartmentID], COUNT(*) AS NoPatient	FROM  [dbo].[Referral] r	GROUP BY r.[DepartmentID]) AS p ON p.[DepartmentID]=m.[DepartmentID]INNER JOIN [dbo].[Department] d ON d.[DepartmentID]=p.[DepartmentID]
+
 
